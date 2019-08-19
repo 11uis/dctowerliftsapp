c896bb75c9f61c398d4f252952999a78dd2b4675
lluis <lluisarco2@gmail.com>
2019-08-19 17:51:05 +0200

    Controller class created

---
+++ DCTowerLiftApp_v01/src/main/java/dctower/lifts/app/Controller.java
@@ -0,0 +1,36 @@
+package dctower.lifts.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private List<Lift> lifts;
+    private List<Employee> employees;
+    private List<Integer> floors;
+    private static Controller ref;
+
+    /**
+     * Private Constructor. -> Singleton-Pattern
+     */
+    private Controller()
+    {
+        // I need to initialize the lists
+        this.lifts = new ArrayList<>();
+        this.employees = new ArrayList<>();
+        this.floors = new ArrayList<>();
+    }
+
+    /**
+     * Singleton-Pattern: the Controller class will be instantiated only once
+     @return Controller
+     */
+    public static synchronized Controller getInstance()
+    {
+        if (ref == null) {
+            ref = new Controller();
+        }
+        return ref;
+    }
+
+
+}
